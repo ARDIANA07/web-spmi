f7593e74eff3f6e962b27fa2b0695dcfbbcd3ee1
Ardiana Abdul Gumelar <aridianaabdul@gmail.com>
2023-07-04 22:25:27 +0700

    add fitur pengguna

--- app/Views/admin/pengguna/index.php
+++ app/Views/admin/pengguna/index.php
@@ -15,43 +15,43 @@
         <a href="/product/new" class="btn btn-outline-dark">New Product</a>
     </div>
 </div>
-
-<div class="row mt-4">
-    <table class="table table-striped w-100" id="productTable">
-        <thead>
-            <tr>
-                <th>No.</th>
-                <th>Nama</th>
-                <th>Jabatan</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $i = 1; ?>
-            <?php foreach ($pengguna as $p) : ?>
-            <tr>
-                <td><?= $i++; ?></td>
-                <td><?= $p['name']; ?></td>
-                <td><?= $p['jabatan']; ?></td>
-                <td>
-                    <div class="row">
-                        <div class="col-auto">
-                            <a href="/product/<?= $p['id_pengguna']; ?>/edit"
-                                class="btn btn-outline-warning mr-2">Edit</a>
+<div class="content">
+    <div class="row mt-4">
+        <table class="table table-striped w-100" id="productTable">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Nama</th>
+                    <th>Jabatan</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $i = 1; ?>
+                <?php foreach ($pengguna as $p) : ?>
+                <tr>
+                    <td><?= $i++; ?></td>
+                    <td><?= $p['name']; ?></td>
+                    <td><?= $p['jabatan']; ?></td>
+                    <td>
+                        <div class="row">
+                            <div class="col-auto">
+                                <a href="/product/<?= $p['id_pengguna']; ?>/edit"
+                                    class="btn btn-outline-warning mr-2">Edit</a>
+                            </div>
+                            <div class="col-auto px-0">
+                                <form action="/product/<?= $p['id_pengguna']; ?>" method="POST">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <button type="submit" class="btn btn-outline-danger"
+                                        onclick="confirm('Apakah kamu yakin akan menghapus data ini ?')">Delete</button>
+                                </form>
+                            </div>
                         </div>
-                        <div class="col-auto px-0">
-                            <form action="/product/<?= $p['id_pengguna']; ?>" method="POST">
-                                <input type="hidden" name="_method" value="DELETE">
-                                <button type="submit" class="btn btn-outline-danger"
-                                    onclick="confirm('Apakah kamu yakin akan menghapus data ini ?')">Delete</button>
-                            </form>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
-
 <?= $this->endSection(); ?>
